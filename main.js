2fa429496cf9e9a836646913dd99b8aca9951281
unknown <yleecruz@gmail.com>
2022-08-31 18:49:30 -0400

    added overlay

--- main.js
+++ main.js
@@ -2,9 +2,12 @@
 
 const el = document.getElementById("navbar-toggle");
 const navbar = document.getElementById("navbar");
+const overlay = document.getElementById("overlay");
 
 const handleToggle = () => {
+	console.log(overlay)
 	navbar.classList.toggle("active");
+	overlay.classList.toggle("active");
 };
 
 el.addEventListener("click", () => handleToggle());
