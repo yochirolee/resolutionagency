f6c6f62e9e1f0631f7d94f7c6fd129a26e4363b5
unknown <yleecruz@gmail.com>
2022-09-01 13:43:58 -0400

    js addes

--- main.js
+++ main.js
@@ -3,10 +3,15 @@
 const el = document.getElementById("navbar-toggle");
 const navbar = document.getElementById("navbar");
 const overlay = document.getElementById("overlay");
+const navbarLinks = document.querySelectorAll(".navbar-link");
 
 const handleToggle = () => {
 	navbar.classList.toggle("active");
 	overlay.classList.toggle("active");
 };
 
+Object.values(navbarLinks).map((link) => {
+	link.addEventListener("click", () => handleToggle());
+});
 el.addEventListener("click", () => handleToggle());
+overlay.addEventListener("click", () => handleToggle());
